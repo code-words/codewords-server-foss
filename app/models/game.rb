0535674cd06eb60ccb01e9dc8d02469851f46c10
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-15 16:07:01 -0600

    Add game validations
    Co-authored-by: Rachael Drennan <rdrennan0@gmail.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ApplicationRecord
   has_many :users, through: :players
 
   has_many :game_cards
-  has_many :cars, through: :game_cards
+  has_many :cards, through: :game_cards
 
   has_many :hints
   has_many :guesses
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { should have_many(:players) }
+    it { should have_many(:users).through(:players) }
+    it { should have_many(:game_cards) }
+    it { should have_many(:cards).through(:game_cards) }
+    it { should have_many(:hints) }
+    it { should have_many(:guesses) }
+  end
 end
