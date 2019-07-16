ac78b65daffe7be1174a3405405c38b0b7f2c24f
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-15 21:33:14 -0600

    Add route to create game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'api/v1', path: 'api/v1' do
+    post '/games', to: 'games#create', as: :create_game
+  end
 end
