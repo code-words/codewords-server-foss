36bffcafdccc09d5350ec9f69a203d4beb874a14
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-16 11:26:23 -0600

    Add controller logic for joining game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -18,6 +18,22 @@ class Api::V1::GamesController < Api::V1::ApiController
 
   def join
     if params[:name]
+      game = Game.includes(:users).find_by(invite_code: params[:invite_code])
+      if game.nil?
+        render_error message: "That invite code is invalid"
+      elsif game.full?
+        render_error message: "That game is already full"
+      elsif game.username_taken? params[:name]
+        render_error message: "That username is already taken"
+      else
+        user = User.create(name: params[:name])
+        player = game.players.create(user: user)
+        render status: 200, json: {
+          game_channel: "game_#{game.game_key}",
+          name: user.name,
+          token: player.token
+        }
+      end
     else
       render_error message: "You must provide a username"
     end
