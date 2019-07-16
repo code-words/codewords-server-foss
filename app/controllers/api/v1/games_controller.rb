78b317f8a0cad7331d8880c5e6f3d78ab40775ad
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-15 22:01:07 -0600

    Add logic for creating game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,4 +1,19 @@
 class Api::V1::GamesController < Api::V1::ApiController
   def create
+    if params[:name]
+      user = User.create(name: params[:name])
+      game = Game.create()
+      player = game.players.create(user: user)
+      render status: 201, json: {
+        game_channel: "game_#{game.game_key}",
+        invite_code: game.invite_code,
+        name: user.name,
+        token: player.token
+      }
+    elsif
+      render status: 401, json: {
+        error: "You must provide a username"
+      }
+    end
   end
 end
