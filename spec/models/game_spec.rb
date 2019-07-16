3b98140573d55c191137498b3ef53675192b7ddc
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-16 13:58:47 -0600

    Add tests for new Game instance methods

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -15,4 +15,44 @@ RSpec.describe Game, type: :model do
     it { should have_many(:hints) }
     it { should have_many(:guesses) }
   end
+
+  describe "Instance Methods" do
+    let(:game){ Game.create() }
+    let(:player1){ User.create(name: "Archer")}
+    let(:player2){ User.create(name: "Lana")}
+    let(:player3){ User.create(name: "Cyril")}
+    let(:player4){ User.create(name: "Cheryl")}
+
+    describe '.username_taken?' do
+      it 'returns false if the username is not yet taken' do
+        game.users += [player1, player2, player3]
+        result = game.username_taken? player4.name
+
+        expect(result).to eq(false)
+      end
+
+      it 'returns true if another player on the game has the given username' do
+        game.users += [player1, player2, player3]
+        result = game.username_taken? player2.name
+
+        expect(result).to eq(true)
+      end
+    end
+
+    describe '.full?' do
+      it 'returns false if the game is not yet full' do
+        game.users += [player1, player2, player3]
+        result = game.full?
+
+        expect(result).to eq(false)
+      end
+
+      it 'returns true if the game is full' do
+        game.users += [player1, player2, player3, player4]
+        result = game.full?
+
+        expect(result).to eq(true)
+      end
+    end
+  end
 end
