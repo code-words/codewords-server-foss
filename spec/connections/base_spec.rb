e1a622685313dd2dbd1ae769eea1a1240ee9ad72
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-17 10:00:52 -0600

    Add tests for creating connection

---
+++ spec/connections/base_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe ApplicationCable::Connection, type: :channel do
+  it 'successfully connects' do
+    user = User.create(name: "Archer")
+    game = Game.create()
+    player = game.players.create(user: user)
+    connect '/cable', params: { token: player.token }
+    expect(connection.current_player).to eq(player)
+  end
+
+  it 'rejects connection' do
+    expect { connect '/cable'}.to have_rejected_connection
+  end
+end
