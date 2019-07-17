68329953c708a1c4ac14cace2658ea8f22f7dbcb
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-17 10:06:59 -0600

    Remove spike-related files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
   scope module: 'api/v1', path: 'api/v1' do
     post '/games', to: 'games#create', as: :create_game
     post '/games/:invite_code/players', to: 'games#join', as: :join_game
