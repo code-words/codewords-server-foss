b833275c0e29c2f21f6eab2c0851133443ffc078
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-17 10:02:47 -0600

    Add Connection logic

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_player
+    end
+
+    private
+      def find_player
+        if player = Player.find_by(token: request.params[:token])
+          player
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
