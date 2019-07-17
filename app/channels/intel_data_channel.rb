ceb2d4d8c98bf2e98e1bf65b340ed60c952cf4d5
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-17 10:03:15 -0600

    Add IntelDataChannel

--- app/channels/intel_data_channel.rb
+++ app/channels/intel_data_channel.rb
@@ -1,6 +1,11 @@
 class IntelDataChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "intel_data_channel"
+    player = Player.includes(:game, :user).find_by(token: params[:token])
+    if player&.role == "intel"
+      stream_from "intel_#{player.game.intel_key}"
+    else
+      reject
+    end
   end
 
   def unsubscribed
