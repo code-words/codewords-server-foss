d1cc603144dcda8855146ae6f090eeddce6dd931
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-19 16:59:05 -0600

    Add route to get intel

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   scope module: 'api/v1', path: 'api/v1' do
     post '/games', to: 'games#create', as: :create_game
     post '/games/:invite_code/players', to: 'games#join', as: :join_game
+    get '/intel', to: 'intel#show', as: :get_intel
   end
 end
