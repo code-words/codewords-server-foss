615f405d531e72b64fd93e9797ffc8bb51063ca8
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-19 19:46:07 -0600

    Add tests to ensure game is initial state is established properly

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -17,11 +17,15 @@ RSpec.describe Game, type: :model do
   end
 
   describe "Instance Methods" do
-    let(:game){ Game.create() }
+    let(:game){ Game.create }
     let(:player1){ User.create(name: "Archer")}
     let(:player2){ User.create(name: "Lana")}
     let(:player3){ User.create(name: "Cyril")}
     let(:player4){ User.create(name: "Cheryl")}
+
+    before(:all) do
+      require './db/seeds/cards'
+    end
 
     describe '.username_taken?' do
       it 'returns false if the username is not yet taken' do
@@ -52,6 +56,46 @@ RSpec.describe Game, type: :model do
         result = game.full?
 
         expect(result).to eq(true)
+      end
+    end
+
+    describe '.establish!' do
+      it 'creates a game board with cards' do
+        game.users += [player1, player2, player3, player4]
+        game.establish!
+        cards = game.game_cards.to_a
+
+        expect(cards.count).to eq(25)
+
+        bystanders = cards.select{|card| card.bystander?}
+        expect(bystanders.count).to eq(7)
+
+        assassin = cards.select{|card| card.assassin?}
+        expect(assassin.count).to eq(1)
+      end
+
+      it 'assigns players equally to red and blue teams' do
+        game.users += [player1, player2, player3, player4]
+        game.establish!
+        players = game.players.to_a
+
+        red_players = players.select{|player| player.red?}
+        blue_players = players.select{|player| player.blue?}
+
+        if players.count.odd?
+          expect(abs(red_players.count - blue_players.count)).to eq(1)
+        else
+          expect(red_players.count).to eq(blue_players.count)
+        end
+      end
+
+      it 'assigns one player on each team to be "intel"' do
+        game.users += [player1, player2, player3, player4]
+        game.establish!
+        players = game.players.to_a
+
+        expect(players.one?{|player| player.red? && player.intel?}).to eq(true)
+        expect(players.one?{|player| player.blue? && player.intel?}).to eq(true)
       end
     end
   end
