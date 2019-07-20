d4cf5695207cae99804ee72d2d7995b8df8ff807
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-19 19:45:29 -0600

    Remove expectation for validating boolean

--- spec/models/game_card_spec.rb
+++ spec/models/game_card_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe GameCard, type: :model do
   describe "Validations" do
     it { should validate_presence_of(:category) }
-    it { should validate_presence_of(:chosen) }
     it { should validate_numericality_of(:address) }
   end
 
