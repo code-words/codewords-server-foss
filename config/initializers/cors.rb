7c2840c53ab0946a27768c8caf0e21712dd747f3
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-21 14:26:58 -0600

    Set CORS for different environments

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,35 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+  if Rails.env.development? || Rails.env.test?
+    allow do
+      origins '*'
 
-    resource '*',
+      resource '*',
       headers: :any,
       methods: :any
+    end
+  elsif Rails.env.production? && ENV['HEROKU_STAGING']
+    allow do
+      origins 'localhost', 'localhost:*', 'https://codewords-game-staging.herokuapp.com'
+
+      resource '/api/*',
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    end
+
+    allow do
+      origins '*'
+
+      resource '/',
+      headers: :any,
+      methods: [:get, :options, :head]
+    end
+  elsif Rails.env.production?
+    allow do
+      origins 'http://playcodewords.com', 'http://www.playcodewords.com'
+
+      resource '/api/*',
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    end
   end
 end
