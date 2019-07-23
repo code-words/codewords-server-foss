972a582ed423bd3a8cee39e74c42addb94d0fc44
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-23 09:39:20 -0600

    Add test for rejecting guess by non-spy player

--- spec/channels/game_data_channel_guesses_spec.rb
+++ spec/channels/game_data_channel_guesses_spec.rb
@@ -76,4 +76,32 @@ describe GameDataChannel, type: :channel do
         expect(payload[:byPlayerId]).to eq(intel.id)
       }
   end
+
+  it 'rejects a guess if sending player does not have spy role' do
+    random_player = @game.players.create(user: User.create(name: "Cheryl"), role: :intel)
+    stub_connection current_player: random_player
+    subscription = subscribe
+
+    built_player = Player.find(random_player.id)
+    built_player.update(role: :intel)
+    @game.current_player = built_player
+    @game.save
+    teammate = @game.players.where(team: random_player.team).where.not(id: random_player.id)
+    teammate.update(role: :spy)
+
+    guess_card = @game.game_cards.where(category: built_player.team).first
+
+    expect{subscription.send_guess(id: guess_card.id)}
+      .to have_broadcasted_to(@game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("#{random_player.name} attempted to submit a guess, but doesn't have the Spy role")
+        expect(payload[:byPlayerId]).to eq(random_player.id)
+      }
+  end
 end
