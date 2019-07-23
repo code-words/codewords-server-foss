751a6d48ffd2f42f4b994f6709bc67333eb3a719
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-22 19:11:44 -0600

    Add additional hint sad-path test cases

--- spec/channels/game_data_channel_hints_spec.rb
+++ spec/channels/game_data_channel_hints_spec.rb
@@ -54,8 +54,60 @@ describe GameDataChannel, type: :channel do
         expect(message[:type]).to eq("illegal-action")
 
         payload = message[:data]
-        expect(payload).to have_key(:error)
-        expect(payload).to have_key(:byPlayerId)
+        expect(payload[:error]).to eq("#{intel.name} attempted to submit a hint out of turn")
+        expect(payload[:byPlayerId]).to eq(intel.id)
+      }
+  end
+
+  it 'rejects a hint if sending player does not have intel role' do
+    random_player = @game.players.create(user: User.create(name: "Cheryl"), role: :spy)
+    stub_connection current_player: random_player
+    subscription = subscribe
+
+    built_player = Player.find(random_player.id)
+    built_player.update(role: :spy)
+    @game.current_player = built_player
+    @game.save
+    teammate = @game.players.where(team: random_player.team).where.not(id: random_player.id)
+    teammate.update(role: :intel)
+
+    expect{subscription.send_hint(hintWord: "Bob", numCards: 3)}
+      .to have_broadcasted_to(@game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("#{random_player.name} attempted to submit a hint, but doesn't have the Intel role")
+        expect(payload[:byPlayerId]).to eq(random_player.id)
+      }
+  end
+
+  it 'rejects a hint if sending player is not current player' do
+    intel = @game.players.create(user: User.create(name: "Cheryl"), role: :intel)
+    stub_connection current_player: intel
+    subscription = subscribe
+
+    built_player = Player.find(intel.id)
+    built_player.update(role: :intel)
+    @game.current_player = built_player
+    @game.save
+    teammate = @game.players.where(team: intel.team).where.not(id: intel.id)
+    teammate.update(role: :spy)
+
+    expect{subscription.send_hint(hintWord: "Bob Loblaw", numCards: 3)}
+      .to have_broadcasted_to(@game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("#{intel.name} attempted to submit an invalid hint")
+        expect(payload[:byPlayerId]).to eq(intel.id)
       }
   end
 end
