b72fd463fb1c8bbb5806023ceb0b64f773b31653
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-23 09:46:50 -0600

    Add test for rejecting guess for card outside game

--- spec/channels/game_data_channel_guesses_spec.rb
+++ spec/channels/game_data_channel_guesses_spec.rb
@@ -11,11 +11,11 @@ describe GameDataChannel, type: :channel do
   end
 
   it 'broadcasts a valid guess to all players' do
-    intel = @game.players.create(user: User.create(name: "Cheryl"), role: :spy, team: :red)
-    stub_connection current_player: intel
+    spy = @game.players.create(user: User.create(name: "Cheryl"), role: :spy, team: :red)
+    stub_connection current_player: spy
     subscription = subscribe
 
-    built_player = Player.find(intel.id)
+    built_player = Player.find(spy.id)
     built_player.update(role: :spy)
     @game.current_player = built_player
     @game.guesses_remaining = 3
@@ -53,11 +53,11 @@ describe GameDataChannel, type: :channel do
   end
 
   it 'rejects a guess if sending player is not current player' do
-    intel = @game.players.create(user: User.create(name: "Cheryl"), role: :intel)
-    stub_connection current_player: intel
+    spy = @game.players.create(user: User.create(name: "Cheryl"), role: :intel)
+    stub_connection current_player: spy
     subscription = subscribe
 
-    @game.current_player = Player.where.not(id: intel.id).first
+    @game.current_player = Player.where.not(id: spy.id).first
     @game.guesses_remaining = 1
     @game.save
 
@@ -72,8 +72,8 @@ describe GameDataChannel, type: :channel do
         expect(message[:type]).to eq("illegal-action")
 
         payload = message[:data]
-        expect(payload[:error]).to eq("#{intel.name} attempted to submit a guess out of turn")
-        expect(payload[:byPlayerId]).to eq(intel.id)
+        expect(payload[:error]).to eq("#{spy.name} attempted to submit a guess out of turn")
+        expect(payload[:byPlayerId]).to eq(spy.id)
       }
   end
 
@@ -100,8 +100,41 @@ describe GameDataChannel, type: :channel do
         expect(message[:type]).to eq("illegal-action")
 
         payload = message[:data]
-        expect(payload[:error]).to eq("#{random_player.name} attempted to submit a guess, but doesn't have the Spy role")
-        expect(payload[:byPlayerId]).to eq(random_player.id)
+        expect(payload[:error]).to eq("#{built_player.name} attempted to submit a guess, but doesn't have the Spy role")
+        expect(payload[:byPlayerId]).to eq(built_player.id)
+      }
+  end
+
+  it 'rejects a guess if guessed card id does not match a card for the game' do
+    spy = @game.players.create(user: User.create(name: "Cheryl"), role: :spy, team: :red)
+    stub_connection current_player: spy
+    subscription = subscribe
+
+    built_player = Player.find(spy.id)
+    built_player.update(role: :spy)
+    @game.current_player = built_player
+    @game.guesses_remaining = 3
+    @game.save
+    teammate = Player.where(game: @game, team: built_player.team).where.not(id: built_player.id).first
+    teammate.update(role: :intel)
+
+    card_ids = @game.game_cards.pluck(:id)
+    guess_id = 1
+    until !card_ids.include? guess_id
+      guess_id = Random.rand(499) + 1 # ensure not 0
+    end
+
+    expect{subscription.send_guess(id: guess_id)}
+      .to have_broadcasted_to(@game)
+      .from_channel(GameDataChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("illegal-action")
+
+        payload = message[:data]
+        expect(payload[:error]).to eq("#{built_player.name} attempted to submit a guess for a card not in this game")
+        expect(payload[:byPlayerId]).to eq(built_player.id)
       }
   end
 end
