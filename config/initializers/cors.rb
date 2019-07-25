4486b1282896d9bef0aabb3aef5ad01c110ebc67
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-24 18:09:04 -0600

    Allow sockets through CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,6 +17,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
+      origins 'localhost', 'localhost:*', 'https://codewords-game-staging.herokuapp.com'
+
+      resource '/cable/*',
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    end
+
+    allow do
       origins '*'
 
       resource '/',
@@ -28,6 +36,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'http://playcodewords.com', 'http://www.playcodewords.com'
 
       resource '/api/*',
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+    end
+
+    allow do
+      origins 'http://playcodewords.com', 'http://www.playcodewords.com'
+
+      resource '/cable/*',
       headers: :any,
       methods: [:get, :post, :options, :head]
     end
